0901d03524bf4bfc5412940c3b62c2994b73734f
Travis Keep <travis_keep@symantec.com>
2016-10-12 13:35:45 -0700

    Add documentation for Go RPC

--- doc.go
+++ doc.go
@@ -1,20 +1,20 @@
 /*
-Package scotty collects endpoint health metrics asynchronously. Although the
-GO API in this package is subject to change at anytime, the REST API that
-the scotty application provides will be stable.
+Package scotty collects endpoint health metrics asynchronously. The scotty
+code and the REST API that the scotty application provides are subject to
+change.
 
 Fetching metrics from scotty using REST API
 
 REST urls (always use GET)
 
-	http://scottyhostname.com/api/hosts/someHostName/someAppName
+	http://scottyhostname.com:6980/api/hosts/someHostName/someAppName
 		Returns every metric from someAppName on someHostName
 		along with values for the last hour.
-	http://scottyhostname.com/api/hosts/someHostName/someAppName/a/path
+	http://scottyhostname.com:6980/api/hosts/someHostName/someAppName/a/path
 		Returns every metric under /a/path from someAppName on
 		someHostName along with values for the last hour.
 		If no metrics match, returns an empty list.
-	http://scottyhostname.com/api/errors
+	http://scottyhostname.com:6980/api/errors
 		Returns a list of every endpoint that scotty cannot currently
 		reach along with the timestamp of the last attempt and the
 		error encountered. If no errors, returns an empty list.
@@ -93,5 +93,39 @@ Sample JSON for api/error call
 	]
 
 For more information on the json schema, see the scotty/messages package
+
+Scotty GO RPC
+
+Scotty GO RPC is available on the same port as the REST API, usually 6980.
+You can see these methods by visiting http://scottyhostname.com:6980/debug/rpc
+
+Scotty.Latest
+
+Gets the latest available metrics under a given path for all active endpoints.
+Pass the absolute path as the input. Empty string ("") means get all the
+latest available metrics. "/foo" means get the metric "/foo" from each
+endpoint or any metric underneath "/foo" from each endpoint.
+The output are the latest metrics as []*messages.LatestMetric.
+The output are sorted first by hostname then by application name and then by
+metric path.
+
+GO RPC Example code:
+
+	import "fmt"
+	import "github.com/Symantec/scotty/messages"
+	import "log"
+	import "net/rpc"
+
+	client, _ := rpc.DialHTTP("tcp", "a.hostname.net:6980")
+	defer client.Close()
+	var latest []*messages.LatestMetric
+	if err := client.Call("Scotty.Latest", "/path/to/metrics", &latest); err != nil {
+		log.Fatal(err)
+	}
+	for _, m := range latest {
+		fmt.Println("Path: ", m.Path)
+		fmt.Println("Timestamp: ", m.Timestamp)
+		fmt.Println("Value: ", m.Value)
+	}
 */
 package scotty

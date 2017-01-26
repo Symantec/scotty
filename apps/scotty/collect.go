d23bf98e4e97e85a6c30c48d1de23bf13e6a992a
Travis Keep <travis_keep@symantec.com>
2017-01-25 17:04:59 -0800

    Multiple goroutines to write to CIS

--- apps/scotty/collect.go
+++ apps/scotty/collect.go
@@ -38,6 +38,10 @@ var (
 		"cisRegex",
 		"",
 		"If provided, host must match regex for scotty to send its data to CIS")
+	fCisConcurrency = flag.Uint(
+		"cisConcurrency",
+		10,
+		"Number of goroutines writing to CIS")
 )
 
 type byHostName messages.ErrorList
@@ -345,77 +349,6 @@ func startCollector(
 	}()
 
 	if cisQueue != nil && cisClient != nil {
-		if err := tricorder.RegisterMetric(
-			"cis/queueSize",
-			cisQueue.Len,
-			units.None,
-			"Length of queue"); err != nil {
-			log.Fatal(err)
-		}
-		timeBetweenWritesDist := tricorder.NewGeometricBucketer(1, 100000.0).NewCumulativeDistribution()
-		if err := tricorder.RegisterMetric(
-			"cis/timeBetweenWrites",
-			timeBetweenWritesDist,
-			units.Second,
-			"elapsed time between CIS updates"); err != nil {
-			log.Fatal(err)
-		}
-		writeTimesDist := tricorder.NewGeometricBucketer(1, 100000.0).NewCumulativeDistribution()
-		if err := tricorder.RegisterMetric(
-			"cis/writeTimes",
-			writeTimesDist,
-			units.Millisecond,
-			"elapsed time between CIS updates"); err != nil {
-			log.Fatal(err)
-		}
-		var lastWriteError string
-		if err := tricorder.RegisterMetric(
-			"cis/lastWriteError",
-			&lastWriteError,
-			units.None,
-			"Last CIS write error"); err != nil {
-			log.Fatal(err)
-		}
-		var successfulWrites uint64
-		if err := tricorder.RegisterMetric(
-			"cis/successfulWrites",
-			&successfulWrites,
-			units.None,
-			"Successful write count"); err != nil {
-			log.Fatal(err)
-		}
-		var totalWrites uint64
-		if err := tricorder.RegisterMetric(
-			"cis/totalWrites",
-			&totalWrites,
-			units.None,
-			"total write count"); err != nil {
-			log.Fatal(err)
-		}
-
-		// CIS loop
-		go func() {
-			lastTimeStampByKey := make(map[interface{}]time.Time)
-			for {
-				stat := cisQueue.Remove().(*cis.Stats)
-				key := stat.Key()
-				if lastTimeStamp, ok := lastTimeStampByKey[key]; ok {
-					timeBetweenWritesDist.Add(stat.TimeStamp.Sub(lastTimeStamp))
-				} else {
-					// On first write, just use time elapsed since start of
-					// scotty
-					timeBetweenWritesDist.Add(time.Now().Sub(programStartTime))
-				}
-				lastTimeStampByKey[key] = stat.TimeStamp
-				writeStartTime := time.Now()
-				if err := cisClient.Write(stat); err != nil {
-					lastWriteError = err.Error()
-				} else {
-					successfulWrites++
-				}
-				totalWrites++
-				writeTimesDist.Add(time.Since(writeStartTime))
-			}
-		}()
+		createCISWriters(cisQueue, cisClient, *fCisConcurrency)
 	}
 }
--- cis/client.go
+++ cis/client.go
@@ -13,7 +13,7 @@ var (
 	replacer = strings.NewReplacer(".", "_")
 )
 
-func (c *Client) write(stats *Stats) error {
+func (c *Client) write(stats *Stats) (info WriteInfo, err error) {
 
 	type versionSizeType struct {
 		Version string `json:"version"`
@@ -39,22 +39,24 @@ func (c *Client) write(stats *Stats) error {
 
 	buffer := &bytes.Buffer{}
 	encoder := json.NewEncoder(buffer)
-	if err := encoder.Encode(jsonToEncode); err != nil {
-		return err
+	if err = encoder.Encode(jsonToEncode); err != nil {
+		return
 	}
+	payload := buffer.Len()
 	// TODO: Maybe get rid of all the diagnostics in the error messages
 	bufferStr := buffer.String()
 	url := fmt.Sprintf("%s/aws/instances/%s", c.endpoint, stats.InstanceId)
 	resp, err := http.Post(url, "application/json", buffer)
 	if err != nil {
-		return errors.New(err.Error() + ": " + url + ": " + bufferStr)
-		//		return err
+		err = errors.New(err.Error() + ": " + url + ": " + bufferStr)
+		return
 	}
 	defer resp.Body.Close()
 	// Do this way in case we get 201
 	if resp.StatusCode/100 != 2 {
-		return errors.New(resp.Status + ": " + url + ": " + bufferStr)
-		// return errors.New(resp.Status)
+		err = errors.New(resp.Status + ": " + url + ": " + bufferStr)
+		// err = errors.New(resp.Status)
 	}
-	return nil
+	info.PayloadSize = uint64(payload)
+	return
 }

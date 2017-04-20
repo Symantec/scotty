6320ac28f1e0f925e2f917c8884ec80fc018c124
Travis Keep <travis_keep@symantec.com>
2017-04-20 13:08:52 -0700

    Introduce cloudhealth package.
    This package writes data to cloudhealth.

---
+++ cloudhealth/api.go
@@ -0,0 +1,131 @@
+package cloudhealth
+
+import (
+	"github.com/Symantec/scotty/lib/yamlutil"
+	"time"
+)
+
+// FVariable represents a floating point cloudfire variable rolled up over
+// some amount of time
+type FVariable struct {
+	Count uint64
+	Min   float64
+	Max   float64
+	Sum   float64
+}
+
+// IsEmpty returns true if no values have been added to this variable
+func (v FVariable) IsEmpty() bool {
+	return v.Count == 0
+}
+
+// Add adds x to this variable
+func (v *FVariable) Add(x float64) {
+	v.add(x)
+}
+
+// Clear clears out this variable.
+func (v *FVariable) Clear() {
+	*v = FVariable{}
+}
+
+// Avg returns the average of this variable
+func (v FVariable) Avg() float64 {
+	return v.Sum / float64(v.Count)
+}
+
+// IVariable represents an integer cloudfire variable rolled up over
+// some amount of time
+type IVariable struct {
+	Count uint64
+	Min   uint64
+	Max   uint64
+	Sum   uint64
+}
+
+// IsEmpty returns true if no values have been added to this variable
+func (v IVariable) IsEmpty() bool {
+	return v.Count == 0
+}
+
+// Add adds x to this variable
+func (v *IVariable) Add(x uint64) {
+	v.add(x)
+}
+
+// Clear clears out this variable.
+func (v *IVariable) Clear() {
+	*v = IVariable{}
+}
+
+// Avg returns the average of this variable
+func (v IVariable) Avg() uint64 {
+	return v.Sum / v.Count
+}
+
+const InstanceDataPointCount = 12 // 4 variables * (min,max,avg)
+
+// InstanceData contains rolled up data for a particular instance
+type InstanceData struct {
+	InstanceId        string    // The aws instance ID
+	Ts                time.Time // The timestamp at one hour granularity
+	CpuUsedPercent    FVariable
+	MemoryFreeBytes   IVariable
+	MemorySizeBytes   IVariable
+	MemoryUsedPercent FVariable
+}
+
+const FsDataPointCount = 9 // 3 variables * (min,max,avg)
+
+// Maximum datapoints that can be written to cloudhealth at once
+const MaxDataPoints = 1000
+
+// FsData contains rolled up data for a particular file system
+type FsData struct {
+	InstanceId    string    // the aws instance ID
+	MountPoint    string    // The mount point of file system
+	Ts            time.Time // The timestamp at one hour granularity
+	FsSizeBytes   IVariable
+	FsUsedBytes   IVariable
+	FsUsedPercent FVariable
+}
+
+// Config configures the writer
+type Config struct {
+	ApiKey        string `yaml:"apiKey"`
+	DataCenter    string `yaml:"dataCenter"` // e.g us-east-1
+	AccountNumber string `yaml:"accountNumber"`
+	DryRun        bool   `yaml:"dryRun"` // If true, runs in dry run mode
+}
+
+func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type configFields Config
+	return yamlutil.StrictUnmarshalYAML(unmarshal, (*configFields)(c))
+}
+
+func (c *Config) Reset() {
+	*c = Config{}
+}
+
+type Writer struct {
+	config Config
+}
+
+// NewWriter returns a new Writer instance.
+func NewWriter(config Config) *Writer {
+	return &Writer{config: config}
+}
+
+// Write writes the provided data in a single request.
+// Write returns an error if the number of data points exceeds 1000. That is,
+// len(instances)*InstanceDataPointCount + len(fss)*FsDataPointCount >= 1000.
+// Write returns the http response code along with an error if applicable.
+// If the write is parital success, Write returns 200 along with the error.
+// Write will never return a 429 response. If Write receives a 429, it
+// retries using exponential backoff internally until it receives a non
+// 429 response and returns that.
+func (w *Writer) Write(
+	instances []InstanceData, fss []FsData) (
+	responseCode int, err error) {
+	return w.write(instances, fss)
+}
---
+++ cloudhealth/writer.go
@@ -0,0 +1,249 @@
+package cloudhealth
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Symantec/scotty/lib/httputil"
+	"net/http"
+	"time"
+)
+
+var (
+	kErrTooManyPoints = errors.New("Too many data points")
+)
+
+type metaDataType struct {
+	AssetType   string   `json:"assetType"`
+	Granularity string   `json:"granularity"`
+	Keys        []string `json:"keys"`
+}
+
+type datasetType struct {
+	Metadata *metaDataType   `json:"metadata"`
+	Values   [][]interface{} `json:"values"`
+}
+
+type metricType struct {
+	Datasets []datasetType `json:"datasets"`
+}
+
+type requestType struct {
+	Metrics metricType `json:"metrics"`
+}
+
+var kInstance = &metaDataType{
+	AssetType:   "aws:ec2:instance",
+	Granularity: "hour",
+	Keys: []string{
+		"assetId",
+		"timestamp",
+		"cpu:used:percent.avg",
+		"cpu:used:percent.max",
+		"cpu:used:percent.min",
+		"memory:free:bytes.avg",
+		"memory:free:bytes.max",
+		"memory:free:bytes.min",
+		"memory:size:bytes.avg",
+		"memory:size:bytes.max",
+		"memory:size:bytes.min",
+		"memory:used:percent.avg",
+		"memory:used:percent.max",
+		"memory:used:percent.min",
+	},
+}
+
+var kFs = &metaDataType{
+	AssetType:   "aws:ec2:instance:fs",
+	Granularity: "hour",
+	Keys: []string{
+		"assetId",
+		"timestamp",
+		"fs:size:bytes.avg",
+		"fs:size:bytes.max",
+		"fs:size:bytes.min",
+		"fs:used:bytes.avg",
+		"fs:used:bytes.max",
+		"fs:used:bytes.min",
+		"fs:used:percent.avg",
+		"fs:used:percent.max",
+		"fs:used:percent.min",
+	},
+}
+
+type chResponseType struct {
+	Succeeded int `json:"succeeded"`
+	Failed    int `json:"failed"`
+	Errors    int `json:"errors"`
+}
+
+func (v FVariable) toValues() (avg, max, min interface{}) {
+	if v.Count == 0 {
+		return
+	}
+	return v.Avg(), v.Max, v.Min
+}
+
+func (v IVariable) toValues() (avg, max, min interface{}) {
+	if v.Count == 0 {
+		return
+	}
+	return v.Avg(), v.Max, v.Min
+}
+
+func toTsValue(t time.Time) string {
+	return t.UTC().Format("2006-01-02T15:04:05-07:00")
+}
+
+func (d *InstanceData) toValues(
+	dataCenter, accountNumber string) []interface{} {
+	assetId := fmt.Sprintf(
+		"%s:%s:%s", dataCenter, accountNumber, d.InstanceId)
+	cpuUsedPercentAvg, cpuUsedPercentMax, cpuUsedPercentMin :=
+		d.CpuUsedPercent.toValues()
+	memoryFreeBytesAvg, memoryFreeBytesMax, memoryFreeBytesMin :=
+		d.MemoryFreeBytes.toValues()
+	memorySizeBytesAvg, memorySizeBytesMax, memorySizeBytesMin :=
+		d.MemorySizeBytes.toValues()
+	memoryUsedPercentAvg, memoryUsedPercentMax, memoryUsedPercentMin :=
+		d.MemoryUsedPercent.toValues()
+	return []interface{}{
+		assetId,
+		toTsValue(d.Ts),
+		cpuUsedPercentAvg,
+		cpuUsedPercentMax,
+		cpuUsedPercentMin,
+		memoryFreeBytesAvg,
+		memoryFreeBytesMax,
+		memoryFreeBytesMin,
+		memorySizeBytesAvg,
+		memorySizeBytesMax,
+		memorySizeBytesMin,
+		memoryUsedPercentAvg,
+		memoryUsedPercentMax,
+		memoryUsedPercentMin,
+	}
+}
+
+func (d *FsData) toValues(
+	dataCenter, accountNumber string) []interface{} {
+	assetId := fmt.Sprintf(
+		"%s:%s:%s:%s",
+		dataCenter,
+		accountNumber,
+		d.InstanceId,
+		d.MountPoint)
+	fsSizeBytesAvg, fsSizeBytesMax, fsSizeBytesMin :=
+		d.FsSizeBytes.toValues()
+	fsUsedBytesAvg, fsUsedBytesMax, fsUsedBytesMin :=
+		d.FsUsedBytes.toValues()
+	fsUsedPercentAvg, fsUsedPercentMax, fsUsedPercentMin :=
+		d.FsUsedPercent.toValues()
+	return []interface{}{
+		assetId,
+		toTsValue(d.Ts),
+		fsSizeBytesAvg,
+		fsSizeBytesMax,
+		fsSizeBytesMin,
+		fsUsedBytesAvg,
+		fsUsedBytesMax,
+		fsUsedBytesMin,
+		fsUsedPercentAvg,
+		fsUsedPercentMax,
+		fsUsedPercentMin,
+	}
+}
+
+func (w *Writer) buildRequest(
+	instances []InstanceData, fss []FsData) ([]byte, error) {
+	var metrics []datasetType
+	if len(instances) > 0 {
+		instanceValues := make([][]interface{}, len(instances))
+		for i := range instances {
+			instanceValues[i] = instances[i].toValues(
+				w.config.DataCenter, w.config.AccountNumber)
+		}
+		metrics = append(
+			metrics,
+			datasetType{
+				Metadata: kInstance,
+				Values:   instanceValues,
+			})
+	}
+	if len(fss) > 0 {
+		fsValues := make([][]interface{}, len(fss))
+		for i := range fss {
+			fsValues[i] = fss[i].toValues(
+				w.config.DataCenter, w.config.AccountNumber)
+		}
+		metrics = append(
+			metrics,
+			datasetType{
+				Metadata: kFs,
+				Values:   fsValues,
+			})
+	}
+	return json.MarshalIndent(
+		&requestType{
+			Metrics: metricType{
+				Datasets: metrics,
+			},
+		}, "", "\t")
+}
+
+func (w *Writer) write(
+	instances []InstanceData, fss []FsData) (int, error) {
+	if len(instances)*InstanceDataPointCount+len(fss)*FsDataPointCount > MaxDataPoints {
+		return 0, kErrTooManyPoints
+	}
+	body, err := w.buildRequest(instances, fss)
+	if err != nil {
+		return 0, err
+	}
+	url := httputil.NewUrl(
+		"https://chapi.cloudhealthtech.com/metrics/v1",
+		"api_key",
+		w.config.ApiKey)
+	if w.config.DryRun {
+		url = httputil.AppendParams(
+			url,
+			"dryrun",
+			"true")
+	}
+	urlStr := url.String()
+	var client http.Client
+	resp, err := client.Post(
+		urlStr, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return 0, err
+	}
+	respCode, err := parseResponse(resp)
+	delay := 20 * time.Millisecond
+	for respCode == 429 {
+		time.Sleep(delay)
+		delay *= 2
+		resp, err = client.Post(
+			urlStr, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			return 0, err
+		}
+		respCode, err = parseResponse(resp)
+	}
+	return respCode, err
+}
+
+func parseResponse(resp *http.Response) (int, error) {
+	defer resp.Body.Close()
+	var buffer bytes.Buffer
+	buffer.ReadFrom(resp.Body)
+	if resp.StatusCode/100 != 2 {
+		return resp.StatusCode, errors.New(buffer.String())
+	}
+	var chResponse chResponseType
+	json.Unmarshal(buffer.Bytes(), &chResponse)
+	if chResponse.Failed > 0 || chResponse.Errors > 0 {
+		return resp.StatusCode, errors.New(buffer.String())
+	}
+	return resp.StatusCode, nil
+}

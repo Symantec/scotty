83b7393975840c298c4c5f0c6c0e8e96e9af8a74
Travis Keep <travis_keep@symantec.com>
2017-05-09 13:58:33 -0700

    Introduce cloudwatch package.

---
+++ cloudwatch/api.go
@@ -0,0 +1,53 @@
+// The cloudwatch package writes data to aws cloudwatch
+package cloudwatch
+
+import (
+	"github.com/Symantec/scotty/chpipeline"
+	"github.com/Symantec/scotty/lib/yamlutil"
+)
+
+// AwsRole represents a single AwsRole in a config file
+type AwsRole struct {
+
+	// looks like "123456789012"
+	AccountNumber string `yaml:"accountNumber"`
+
+	// looks like "arn:aws:iam::123456789012:role/SOME-ROLE"
+	RoleArn string `yaml:"roleArn"`
+}
+
+func (a *AwsRole) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type awsRoleFields AwsRole
+	return yamlutil.StrictUnmarshalYAML(unmarshal, (*awsRoleFields)(a))
+}
+
+// Config represents the configuration of a Writer
+type Config struct {
+	Roles []AwsRole `yaml:"roles"`
+
+	// looks like "us-east-1", "us-west-2", etc.
+	Region string `yaml:"region"`
+}
+
+func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type configFields Config
+	return yamlutil.StrictUnmarshalYAML(unmarshal, (*configFields)(c))
+}
+
+func (c *Config) Reset() {
+	*c = Config{}
+}
+
+// A Writer instance writes cloudwatch data to AWS
+type Writer struct {
+	serviceClientsByAccount map[string]*serviceClientsType
+}
+
+func NewWriter(c Config) (*Writer, error) {
+	return newWriter(c)
+}
+
+// Write writes data for a single instance to AWS
+func (w *Writer) Write(snapshot *chpipeline.Snapshot) error {
+	return w.write(snapshot)
+}
---
+++ cloudwatch/writer.go
@@ -0,0 +1,43 @@
+package cloudwatch
+
+import (
+	"fmt"
+	"github.com/Symantec/scotty/chpipeline"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
+type serviceClientsType struct {
+	CloudWatch *cloudwatch.CloudWatch
+}
+
+func newWriter(c Config) (*Writer, error) {
+	sess, err := session.NewSessionWithOptions(
+		session.Options{
+			Config: aws.Config{
+				Region: aws.String(c.Region)}})
+	if err != nil {
+		return nil, err
+	}
+	serviceClientsByAccount := make(
+		map[string]*serviceClientsType, len(c.Roles))
+	for _, awsRole := range c.Roles {
+		creds := stscreds.NewCredentials(sess, awsRole.RoleArn)
+		cwService := cloudwatch.New(sess, &aws.Config{Credentials: creds})
+		serviceClients := &serviceClientsType{CloudWatch: cwService}
+		serviceClientsByAccount[awsRole.AccountNumber] = serviceClients
+	}
+	return &Writer{serviceClientsByAccount: serviceClientsByAccount}, nil
+}
+
+func (w *Writer) write(snapshot *chpipeline.Snapshot) error {
+	serviceClients, ok := w.serviceClientsByAccount[snapshot.AccountNumber]
+	if !ok {
+		return fmt.Errorf("Unrecognizsed account: %s", snapshot.AccountNumber)
+	}
+	_, err := serviceClients.CloudWatch.PutMetricData(
+		toPutMetricData(snapshot))
+	return err
+}
